23fa1807d0100affa73f92944aca1cfe0845eeb5
balavenkat123 <balavenkat17902@gmail.com>
2023-07-05 20:17:19 +0530

    sociallinks

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaLinkedin,FaGithub } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill} from "react-icons/bs";
+
+const SocialLinks = () => {
+  return (
+    <div className=' flex flex-col top-[35%] left-0 fixed '>
+        <ul>
+            <li className='flex justify-between items-center w-40 h-14 px-4 bg-white'>
+                 <a href="">                   
+                    <>
+                        LinkedIn<FaLinkedin/>
+                    </>
+                 </a>
+             </li>
+        </ul>
+    </div>
+  )
+}
+
+export default SocialLinks
