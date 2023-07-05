a012dfecece079da8c3b2452f9443f556daa98b7
balavenkat123 <balavenkat17902@gmail.com>
2023-07-05 20:02:03 +0530

    typed.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroImage from '../assets/HeroImage.png'
 import {CgArrowRight} from 'react-icons/cg'
+import Typed from 'react-typed';
 
 
 
@@ -10,6 +11,7 @@ const Home = () => {
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row' >
         <div className='flex flex-col justify-center h-full' >
           <h2 className='text-4xl sm:text-6xl font-bold text-white'>Hi..i am front end developer</h2>
+          <Typed strings={['model','modle','super','model🤙']} typeSpeed={60} backSpeed={40} loop className='text-3xl sm:text-5xl font-bold text-blue-500'/>
           <p className='text-gray-500 py-4 max-w-md'>i'll edit about me..ohwf.</p>
           <div>
             <button className='group text-black font-bold w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-indigo-300 to-purple-400 cursor-pointer'>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,7 +30,7 @@ const NavBar = () => {
   return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed '>
         <div>
-            <h1 className="text-5xl font-name ml-2">Bala Venkat</h1>
+            <h1 className="text-5xl font-name ml-2">Bala Venkat...</h1>
         </div>
 
         <ul className="hidden md:flex">
