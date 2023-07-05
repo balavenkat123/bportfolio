e78a16a8665a26848c6852caeb628fef59d78ee3
balavenkat123 <balavenkat17902@gmail.com>
2023-07-05 20:41:41 +0530

    iconlg

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,7 +5,7 @@ import { BsFillPersonLinesFill} from "react-icons/bs";
 
 const SocialLinks = () => {
   return (
-    <div className=' flex flex-col top-[35%] left-0 fixed '>
+    <div className=' hidden lg:flex flex-col top-[35%] left-0 fixed '>
         <ul>
             <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-200 ml-[-100px] hover:ml-[-5px] duration-200'>
                  <a href="" className='flex justify-between items-center w-full text-black'>                   
